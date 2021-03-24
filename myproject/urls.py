f21c34edc3b05276a0be09849fc3499994a074f9
Aby Kal <abyabrkal@gmail.com>
2021-03-24 09:23:11 +0400

    URL updated with topics in boards

--- myproject/urls.py
+++ myproject/urls.py
@@ -14,17 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
+# from django.urls import path
 
 from boards import views
 
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('admin/', admin.site.urls),
-]
-
-
 # urlpatterns = [
-#     url(r'^$', views.home, name='home'),
-#     url(r'^admin/', admin.site.urls),
+#     path('', views.home, name='home'),
+#     path('admin/', admin.site.urls),
 # ]
+
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^admin/', admin.site.urls),
+]
