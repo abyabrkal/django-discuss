4f209ffb3c0af1f4def09f2ec63a5d109f1a1ec8
Aby Kal <abyabrkal@gmail.com>
2021-03-24 09:35:55 +0400

    Testing the boards topics

--- boards/views.py
+++ boards/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Board
 
 def home(request):
@@ -7,5 +8,10 @@ def home(request):
 
 
 def board_topics(request, pk):
-    board = Board.objects.get(pk=pk)
+    board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
+    # try:
+    #     board = Board.objects.get(pk=pk)
+    # except Board.DoesNotExist:
+    #     raise Http404
+    # return render(request, 'topics.html', {'board': board})
