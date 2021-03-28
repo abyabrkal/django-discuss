7470e6e92486bf6d087f1a6d9db1ee5fd7e1a7d0
Aby Kal <abyabrkal@gmail.com>
2021-03-28 16:37:04 +0400

    Passowrd Reset Views added with tests

--- myproject/settings.py
+++ myproject/settings.py
@@ -130,5 +130,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+
+LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_REDIRECT_URL = 'home'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
